b6b4f3cdcabb74d377817edcf98adda40d3789d5
Rajkumar Yadav <rajkumaryadav7617@gmail.com>
2022-09-26 22:36:41 +0530

    Adding Some Function

--- LRU/LRU.cpp
+++ LRU/LRU.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
+class Node {
+public:
+	int key, val;
+	Node *next, *prev;
+	Node(int key, int val) {
+		this->key = key;
+		this->val = val;
+		next = prev = nullptr;
+	}
+};
 
+class Solution {
+	Node *head, *tail;
+	int capacity;
+	unordered_map<int, Node*> hashMap;
+public:
+	Solution(int capacity) {
+		head = new Node(-1, -1);
+		tail = new Node(-1, -1);
+		this->capacity = capacity;
+		head->next = tail;
+		tail->prev = head;
+	}
+	void addNode(int key, int val) {
+		Node *nxt = head->next;
+		Node *newNode = new Node(key, val);
+		newNode->next = nxt;
+		nxt->prev = newNode;
+		newNode->prev = head;
+		head->next = newNode;
+	}
+
+	void deleteNode(Node *delNode) {
+		Node *pre = delNode->prev;
+		Node *nxt = delNode->next;
+		pre->next = nxt;
+		nxt->prev = pre;
+		delete delNode;
+	}
+
+	bool size() {
+		return capacity == hashMap.size();
+	}
+};
 
 int main() {
 
